59f48a1fabeeade6e9c5dfea3017f0b980e81c4b
sargassov <sargassov@yandex.ru>
2021-03-05 22:48:06 +0300

    Logging added into a project

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -15,7 +15,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 500, 350));
         primaryStage.show();
     }
-
     public static void main(String[] args) {
         launch(args);
     }
